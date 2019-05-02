e3a790ab0830b1f051a5ef5c908da5f1573e4b1b
Benny Wang <bennywang75@hotmail.com>
2019-05-02 10:45:10 -0700

    Init

---
+++ batch.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+hosts=(
+    1
+    2
+    3
+    4
+    5
+    6
+    7
+    8
+    9
+    10
+    11
+    12
+    13
+    14
+    15
+    16
+    17
+    18
+    190
+    20
+    112
+    22
+    23
+    24
+    25
+)
+
+case $1 in
+    "ping" )
+        for host in ${hosts[*]}
+        do
+            ping -c 1 -W 1 192.168.0.$host > /dev/null
+            if [ $? -eq 0 ]
+            then
+                echo "192.168.0.$host up"
+            else
+                echo "192.168.0.$host down"
+            fi
+        done
+    ;;
+
+    "check" )
+        for host in ${hosts[*]}
+        do
+            nmap -p 22 "192.168.0.$host" | grep "open" > /dev/null
+            if [ $? -eq 0 ]
+            then
+                echo "SSH UP on 192.168.0.$host"
+            else
+                echo "SSH DOWN on 192.168.0.$host"
+            fi
+        done
+    ;;
+
+    "test" )
+        for host in ${hosts[*]}
+        do
+            ssh root@192.168.0.$host "uname -a" &
+        done
+    ;;
+
+    "rst" )
+        for host in ${hosts[*]}
+        do
+            ssh root@192.168.0.$host "shutdown -r now" &
+        done
+    ;;
+
+    "connect" )
+        ssh root@192.168.0.$2
+    ;;
+
+    * )
+        echo "Invalid command"
+    ;;
+esac
